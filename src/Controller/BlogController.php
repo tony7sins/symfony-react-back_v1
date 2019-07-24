5cfc5c1ac21dd49a3a9844927ac46a0e1f93f2fa
t7sg <tonigalczeskij@gmail.com>
2019-07-24 14:59:52 +0300

    35. Deleting Entities

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 // use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
@@ -44,26 +47,6 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
-     */
-    // @ParamConverter("post", class="App:BlogPost")
-    public function post(BlogPost $post)
-    {
-        // Same as find($id)
-        return $this->json($post);
-    }
-
-    /**
-     * @Route("/post/{slug}", name="blog_by_slug")
-     */
-    // @ParamConverter("post", class="App:BlogPost", options={"mapping" : {"slug" : "slug"}})
-    public function postBySlug(BlogPost $post)
-    {
-        // Same as findOneBy(['slug' => $slug])
-        return $this->json($post);
-    }
-
-    /**
      * @Route("/add", name="blog_add", methods={"POST"})
      */
     public function add(
@@ -83,5 +66,37 @@ class BlogController extends AbstractController
         $em->flush();
 
         return $this->json($blogPost);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
+     */
+    // @ParamConverter("post", class="App:BlogPost")
+    public function post(BlogPost $post)
+    {
+        // Same as find($id)
+        return $this->json($post);
+    }
+
+    /**
+     * @Route("/post/{slug}", name="blog_by_slug", methods={"GET"})
+     */
+    // @ParamConverter("post", class="App:BlogPost", options={"mapping" : {"slug" : "slug"}})
+    public function postBySlug(BlogPost $post)
+    {
+        // Same as findOneBy(['slug' => $slug])
+        return $this->json($post);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+     */
+    public function delete(BlogPost $post)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
