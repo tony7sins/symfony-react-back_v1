80124337dd1e649cc764fda1d9fe7b0035a8166a
t7sg <tonigalczeskij@gmail.com>
2019-07-24 15:28:34 +0300

    36. Data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,30 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\BlogPost;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost
+            ->setTitle('A first post!')
+            ->setPublished(new \DateTime('2018-07-25 12:00:00'))
+            ->setContent('Post content')
+            ->setAuthor('Piotr Jura')
+            ->setSlug('a-first-post');
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost
+            ->setTitle('A second post!')
+            ->setPublished(new \DateTime('2018-07-26 14:00:00'))
+            ->setContent('Post content second')
+            ->setAuthor('Jura Piotr')
+            ->setSlug('a-second-post');
+        $manager->persist($blogPost);
+
 
         $manager->flush();
     }
