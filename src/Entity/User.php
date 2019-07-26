1046bee591911532ac76e00407be35ca8d1d873b
t7sg <tonigalczeskij@gmail.com>
2019-07-26 17:43:40 +0300

    52. Virtual property, @Expression validator

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -50,6 +50,15 @@ class User implements UserInterface
     private $password;
 
     /**
+     * Check the password value
+     * @Assert\Expression(
+     *      "this.getPassword() === this.getRetypedPassword()",
+     *      message="Passwords does not matches"
+     * )
+     */
+    private $retypedPassword;
+
+    /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"read"})
      * @Assert\NotBlank()
@@ -174,4 +183,16 @@ class User implements UserInterface
 
     public function eraseCredentials()
     { }
+
+    public function getRetypedPassword()
+    {
+        return $this->retypedPassword;
+    }
+
+    public function setRetypedPassword($retypedPassword): self
+    {
+        $this->retypedPassword = $retypedPassword;
+
+        return $this;
+    }
 }
