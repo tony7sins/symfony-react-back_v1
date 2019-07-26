53b945ceb795e83d73534cae698c922631334781
t7sg <tonigalczeskij@gmail.com>
2019-07-26 23:14:44 +0300

    65.Fix User normalization & password hash subscriber

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,16 +13,35 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
+ *     normalizationContext={"groups"={"get"}},
  *     attributes={"formats"={"jsonld"}},
  *     itemOperations={
  *         "get"={
- *             "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
+ *             "access_control"="is_granted('IS_AUTHENTICATED_FULLY')",
+ *              "normalization_context"={
+ *                   "groups"={"get"}
+ *              }
+ *         },
+ *         "put"={
+ *             "access_control"="is_granted('IS_AUTHENTICATED_FULLY') and object == user",
+ *              "denormalization_context"={
+ *                   "groups"={"put"}
+ *              },
+ *              "normalization_context"={
+ *                  "groups"={"get"}
+ *              }
  *         }
  *     },
- *     collectionOperations={"post"},
- *     normalizationContext={
- *          "groups"={"read"}
- *      }
+ *     collectionOperations={
+ *         "post"={
+ *              "denormalization_context"={
+ *                   "groups"={"post"}
+ *              },
+ *              "normalization_context"={
+ *                  "groups"={"get"}
+ *              }
+ *          }
+ *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity("username")
@@ -35,13 +54,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"read"})
+     * @Groups({"get"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"read"})
+     * @Groups({"get", "post"})
      * @Assert\NotBlank()
      * @Assert\Length(min=6, max=255)
      */
@@ -49,6 +68,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"post", "put"})
      * @Assert\NotBlank()
      * @Assert\Regex(
      *      pattern="/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{7,}/",
@@ -59,6 +79,7 @@ class User implements UserInterface
 
     /**
      * Check the password value
+     * @Groups({"post", "put"})
      * @Assert\Expression(
      *      "this.getPassword() === this.getRetypedPassword()",
      *      message="Passwords does not matches"
@@ -68,7 +89,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"read"})
+     * @Groups({"get", "post", "put"})
      * @Assert\NotBlank()
      * @Assert\Length(min=5, max=255)
      */
@@ -76,6 +97,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"post", "put"})
      * @Assert\NotBlank()
      * @Assert\Email()
      * @Assert\Length(min=5, max=255)
@@ -84,12 +106,13 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\BlogPost", mappedBy="author")
-     * @Groups({"read"})
+     * @Groups({"get"})
      */
     private $posts;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="author")
+     * @Groups({"get"})
      */
     private $comments;
 
