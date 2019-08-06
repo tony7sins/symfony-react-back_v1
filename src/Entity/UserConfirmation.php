4ba3bede3eef5f21ffef9ed1157995b2c3a0516c
t7sg <tonigalczeskij@gmail.com>
2019-08-06 23:37:09 +0200

    13.5. UserConfirmation custom API Resource

---
+++ src/Entity/UserConfirmation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource(
+ *      collectionOperations={
+ *          "post"={
+ *              "path"="/users/confirm"
+ *          }
+ *      }
+ * )
+ */
+class UserConfirmation
+{
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(min=30, max=30)
+     */
+    public $confirmationToken;
+}
