74e7d891adc2a56b5cf3699a8a8be2f4152fd1ba
t7sg <tonigalczeskij@gmail.com>
2019-08-08 18:57:38 +0200

    14.2. Image entity with migration and @Uploadable annotation

---
+++ src/Entity/Image.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity()
+ * @Vich\Uploadable( )
+ * @ApiResource(
+ * 
+ * )
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Vich\UploadableField(mapping="images", fileNameProperty="url")
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    private $url;
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of file
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function setFile($file): void
+    {
+        $this->file = $file;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url): void
+    {
+        $this->url = $url;
+    }
+}
