a0b3f9b072b6202a8526254a3df0f98146f01675
t7sg <tonigalczeskij@gmail.com>
2019-08-09 19:21:42 +0200

    15.5. Sorting filter

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -5,9 +5,10 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiSubresource;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -36,6 +37,17 @@ use Symfony\Component\Validator\Constraints as Assert;
  *      RangeFilter::class,
  *      properties={
  *          "id"
+ *      }
+ * )
+ * @ApiFilter(
+ *      OrderFilter::class,
+ *      properties={
+ *          "id",
+ *          "published",
+ *          "title"
+ *      },
+ *      arguments={
+ *          "orderParameterName"="_order"
  *      }
  * )
  * @ApiResource(
