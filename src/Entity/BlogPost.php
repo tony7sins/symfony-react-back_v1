cba30554bd391b502195050064a88a5c33c93c8e
t7sg <tonigalczeskij@gmail.com>
2019-08-09 19:42:44 +0200

    15.7. Property filter

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -9,6 +9,7 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -49,6 +50,20 @@ use Symfony\Component\Validator\Constraints as Assert;
  *      },
  *      arguments={
  *          "orderParameterName"="_order"
+ *      }
+ * )
+ * @ApiFilter(
+ *      PropertyFilter::class,
+ *      arguments={
+ *          "parameterName": "properties", 
+ *          "overrideDefaultProperties": false, 
+ *          "whitelist": {
+ *              "id",
+ *              "author",
+ *              "slug",
+ *              "title",
+ *              "content"
+ *          }
  *      }
  * )
  * @ApiResource(
