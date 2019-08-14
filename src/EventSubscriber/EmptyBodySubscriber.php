f91c01650594c9fced2b4feccde8259aea3cbd50
t7sg <tonigalczeskij@gmail.com>
2019-08-14 23:51:00 +0200

    20.10. Image upload feature

--- src/EventSubscriber/EmptyBodySubscriber.php
+++ src/EventSubscriber/EmptyBodySubscriber.php
@@ -3,54 +3,40 @@
 namespace App\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Exception\EmptyBodyException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use App\Exception\EmptyBodyException;
 
 class EmptyBodySubscriber implements EventSubscriberInterface
 {
-    public static function getSubscribedEvents(): array
+    public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::REQUEST =>  [
+            KernelEvents::REQUEST => [
                 'handleEmptyBody',
-                EventPriorities::PRE_WRITE
-                // EventPriorities::PRE_VALIDATE
-                // EventPriorities::POST_DESERIALIZE
-                // EventPriorities::PRE_DESERIALIZE
-            ]
+                EventPriorities::POST_DESERIALIZE,
+            ],
         ];
     }
 
-    public function handleEmptyBody(RequestEvent $event)
+    public function handleEmptyBody(GetResponseEvent $event)
     {
         $request = $event->getRequest();
         $method = $request->getMethod();
         $route = $request->get('_route');
 
         if (
-            !in_array(
-                $method,
-                [
-                    Request::METHOD_POST,
-                    Request::METHOD_PUT
-                ]
-            ) ||
-            in_array(
-                $request->getContentType(),
-                [
-                    'html',
-                    'form'
-                ]
-            ) ||
-            substr($route, 0, 3) !== "api"
+            !in_array($method, [Request::METHOD_POST, Request::METHOD_PUT]) ||
+            in_array($request->getContentType(), ['html', 'form']) ||
+            substr($route, 0, 3) !== 'api'
         ) {
             return;
         }
 
-        $data = $event->getRequest()->get('data');
+        $data = $event->getRequest()
+            ->get('data');
 
         if (null === $data) {
             throw new EmptyBodyException();
